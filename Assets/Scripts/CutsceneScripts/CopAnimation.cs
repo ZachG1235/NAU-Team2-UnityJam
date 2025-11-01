4cd4330d4a962fcac5449dd56a0e837b8ac3fcf2
Camp Level Up <camplevelup@MIX358-PC11.asurite.ad.asu.edu>
2025-11-01 16:04:53 -0700

    cutscene

---
+++ Assets/Scripts/CutsceneScripts/CopAnimation.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CopAnimation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
