60c5551433aeafcbce0277934191d010ba6fd975
ZachG1235 <ztg29@nau.edu>
2025-11-02 12:27:25 -0700

    bloopbloop bleep

--- Assets/Scripts/CutsceneScripts/CutsceneText.cs
+++ Assets/Scripts/CutsceneScripts/CutsceneText.cs
@@ -12,6 +12,7 @@ public class CutsceneText : MonoBehaviour
     public float initialDelay = 1f;
     public float ending_seconds_wait_time = 5f;
     public int lvl;
+    public AudioSource text_bloop_sfx;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,12 +31,20 @@ public class CutsceneText : MonoBehaviour
     
     IEnumerator TypeText(string text)
     {
+        int text_index = 0;
         yield return new WaitForSeconds(initialDelay);
         textBox.text = ""; // start empty
 
         foreach (char letter in text.ToCharArray())
         {
             textBox.text += letter; // add one letter
+            if (text_index % 2 == 1)
+            {
+                text_bloop_sfx.Play();
+                float[] pitch_array = { 0.95f, 1f, 1.05f };
+                text_bloop_sfx.pitch = pitch_array[Random.Range(0, 2)];
+            }
+            text_index++;
             yield return new WaitForSeconds(typingSpeed); // wait
         }
 
