b26dfdedb4963ff9e953e0b4f85b20342d7fcb55
Sage Tesoro <satewarco@gmail.com>
2025-11-02 09:57:43 -0700

    hover ugh

--- Assets/Scripts/HoverObject.cs
+++ Assets/Scripts/HoverObject.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class HoverObject : MonoBehaviour {
     private HoverManager hoverManager;
     [SerializeField] private bool touching_player = false;
+    [SerializeField] private bool mouseEnter = false;
     public GameObject player;
     void Start() {
         hoverManager = FindFirstObjectByType<HoverManager>();
         // Finds the UI manager
+    }
+    void Update() {
+        if (touching_player && mouseEnter) {
+            Debug.Log("ahhhhhh");
+            hoverManager.ShowHoverText(transform, "Press E");
+        }
+        if (!touching_player || !mouseEnter) {
+            hoverManager.HideHoverText();
+        }
     }
 
     void OnTriggerEnter(Collider collision) {
@@ -25,16 +35,16 @@ public class HoverObject : MonoBehaviour {
     }
     void OnMouseEnter() {
         Debug.Log("Enter");
-        if (touching_player){
-            hoverManager.ShowHoverText(transform, gameObject.name); }
+        mouseEnter = true;
+
+
 
     }
 
     void OnMouseExit() {
         Debug.Log("exit");
-        if (touching_player) {
-            hoverManager.HideHoverText();
-        }
+
+        mouseEnter = false;
 
     }
 }
